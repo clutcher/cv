608b926005d4237e483f96fb8ebe352e6bfa240c
Igor Zarvanskyi <iclutcher@gmail.com>
2024-02-28 18:27:18 +0100

    Add Software Architect experience at Zaelab (Trek Bicycles)

--- latexdockercmd.sh
+++ latexdockercmd.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 IMAGE=blang/latex:ubuntu
-exec docker run --rm -i --user="$(id -u):$(id -g)" --net=none -v "$PWD":/data "$IMAGE" latexmk -cd -f -interaction=batchmode -pdf cv_java_zarvanskiy.tex
+exec docker run --rm -i --user="$(id -u):$(id -g)" --net=none -v "$PWD":/data "$IMAGE" latexmk -cd -f -interaction=batchmode -pdf cv_java_zarvanskyi.tex
