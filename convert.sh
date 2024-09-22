573f6cf2395af7415a955a82c2cbf144e3315675
Igor Zarvanskyi <iclutcher@gmail.com>
2024-09-23 00:27:45 +0200

    Extract file name into separate variable

--- convert.sh
+++ convert.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 IMAGE=pandoc/extra
 
-docker run --rm -i --user="$(id -u):$(id -g)" --net=none -v "$PWD":/data "$IMAGE" resume.md --pdf-engine=lualatex -s --template resume-style.tex -o igor_zarvanskyi_sap_commerce.pdf
+OUTPUT_FILE_NAME=igor_zarvanskyi_sap_commerce.pdf
+
+docker run --rm -i --user="$(id -u):$(id -g)" --net=none -v "$PWD":/data "$IMAGE" resume.md --pdf-engine=lualatex -s --template resume-style.tex -o $OUTPUT_FILE_NAME
 echo "Conversion to pdf finished"
